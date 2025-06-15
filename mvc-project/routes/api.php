b237453fe6e04268a81500d066d33135e747143a
uazmi91 <uazmi910@gmal.com>
2025-06-15 14:16:41 +0700

    update tugas 6

--- mvc-project/routes/api.php
+++ mvc-project/routes/api.php
@@ -12,3 +12,21 @@ Route::post('/genres', [GenreController::class, 'store']);
 
 Route::get('/authors', [AuthorController::class, 'index']);
 Route::post('/authors', [AuthorController::class, 'store']);
+
+// Public routes (GET only)
+Route::get('/genres', [GenreController::class, 'index']);
+Route::get('/genres/{id}', [GenreController::class, 'show']);
+
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::get('/authors/{id}', [AuthorController::class, 'show']);
+
+// Protected routes (admin only)
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::post('/genres', [GenreController::class, 'store']);
+    Route::put('/genres/{id}', [GenreController::class, 'update']);
+    Route::delete('/genres/{id}', [GenreController::class, 'destroy']);
+
+    Route::post('/authors', [AuthorController::class, 'store']);
+    Route::put('/authors/{id}', [AuthorController::class, 'update']);
+    Route::delete('/authors/{id}', [AuthorController::class, 'destroy']);
+});
